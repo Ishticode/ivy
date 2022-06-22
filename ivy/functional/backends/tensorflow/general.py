ae3642b1601390b13cf25b14df246b3b04fa3e45
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-06-22 04:29:51 +0100

    cleaning up in tf

--- ivy/functional/backends/tensorflow/general.py
+++ ivy/functional/backends/tensorflow/general.py
@@ -361,45 +361,35 @@ get_num_dims = (
 #         ret = tf.map_fn(fun, args[i])
 
 
-
 def vmap(fun, in_axis=0, out_axis=0):
     @ivy.to_native_arrays_and_back
     def _vmap(*args):
-        # put in_axis at 0 as tf.map_fn unstacks the batch along axis 0.
 
-        # require mutability in the arg container.
-
+        # convert args tuple to list to allow mutability in the arg container.
         args = list(args)
 
+        # if in_axis is a non-integer, its length should be equal to pos args.
         if isinstance(in_axis, (list, tuple)):
             try:
                 assert (len(args)) == len(in_axis)
-            except:
+            except AssertionError:
                 raise Exception('''The in_axis should have length equivalent to the 
                 number of positional arguments to the function being vectorized
                 or it shold be an intger.''')
 
-        # if in_axis:
-        #     batch = args[0]
-        #     batch = tf.experimental.numpy.moveaxis(batch, in_axis, 0)
-
-        # apply vectorisation
+        # set up the axis to be mapped
         if isinstance(in_axis, (tuple, list)):
             for i in range(len(in_axis)):
                 args[i] = tf.experimental.numpy.moveaxis(args[i], in_axis[i], 0)
         elif isinstance(in_axis, int):
             args[0] = tf.experimental.numpy.moveaxis(args[0], in_axis, 0)
 
-        #func = partial(tf.map_fn(fun, args[0]))
-
+        # vecotrisation - applying map_fn if only one arg provided as reduce requires
+        # two elements to begin with.
         if len(args) == 1:
-            ret = (
-                tf.map_fn(fun, args[0])
-            )
+            ret = tf.map_fn(fun, args[0])
         else:
-            ret = (
-                (reduce(fun, args))
-            )
+            ret = reduce(fun, args)
 
         if out_axis:
             ret = tf.experimental.numpy.moveaxis(ret, 0, out_axis)
