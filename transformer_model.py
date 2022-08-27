6246c0adb31cd6905682034593edfe62927669ea
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-08-27 18:26:23 +0100

    calling functions in ivy namespace

--- transformer_model.py
+++ transformer_model.py
@@ -1,13 +1,11 @@
 import ivy
-from ivy import Container, Linear, MultiHeadAttention, Module, Sequential, Embedding, Dropout, LayerNorm
 
-
-# class TransformerConfig(Container):
+# class TransformerConfig(ivy.Container):
 #     def __init__(self, stacks, heads, dim, dropout, device=None, v=None):
 #         super().__init__()
 
 
-class Encoder(Module):
+class Encoder(ivy.Module):
 
     def __init__(self, embed_dim, num_heads, dropout, device=None, v=None):
         self.embed_dim = embed_dim
@@ -16,19 +14,19 @@ class Encoder(Module):
         self.dropout = dropout
         self.device = device
         self.v = v
-        self.attention = MultiHeadAttention(self.embed_dim,
+        self.attention = ivy.MultiHeadAttention(self.embed_dim,
                                             self.num_heads,
                                             self.head_dim,
                                             self.dropout,
                                             self.device,
                                             self.v)
-        self.ffn = Sequential(
-            Linear(self.embed_dim, 4 * self.head_dim),
-            Linear(4 * self.head_dim, self.embed_size),
+        self.ffn = ivy.Sequential(
+            ivy.Linear(self.embed_dim, 4 * self.head_dim),
+            ivy.Linear(4 * self.head_dim, self.embed_size),
         )
-        self.dropout = Dropout(dropout)
-        self.Norm = LayerNorm(self.embed_dim)
-        ivy.Module.__init__(
+        self.dropout = ivy.Dropout(dropout)
+        self.Norm = ivy.LayerNorm(self.embed_dim)
+        ivy.ivy.Module.__init__(
             self,
             device,
             v)
@@ -41,14 +39,14 @@ class Encoder(Module):
         return y
 
 
-class TransformerEncoder(Module):
+class TransformerEncoder(ivy.Module):
     def __init__(self, vocab_size, embed_size, num_stacks, num_heads, drop_out, maxlength, device=None, v=None):
         self.device = device
-        self.embedding = Embedding(vocab_size, embed_size)
-        self.pos_embedding = Embedding(maxlength, embed_size)
-        self.blocks = Sequential(
+        self.embedding = ivy.Embedding(vocab_size, embed_size)
+        self.pos_embedding = ivy.Embedding(maxlength, embed_size)
+        self.blocks = ivy.Sequential(
             *[Encoder(embed_size, num_heads, drop_out, device, v) for _ in range(num_stacks)])
-        ivy.Module.__init__(
+        ivy.ivy.Module.__init__(
             self,
             device,
             v)
@@ -61,13 +59,13 @@ class TransformerEncoder(Module):
         return output
 
 
-class DecoderBlock(Module):
+class DecoderBlock(ivy.Module):
 
     def __init__(self, embed_size, mask, num_heads = 8, drop_out=0.1, device=None, v=None):
         self.mask = mask
-        self.dropout = Dropout(drop_out)
-        self.norm = LayerNorm(embed_size)
-        self.attn = MultiHeadAttention(embed_size, num_heads, embed_size // num_heads, drop_out, device, v)
+        self.dropout = ivy.Dropout(drop_out)
+        self.norm = ivy.LayerNorm(embed_size)
+        self.attn = ivy.MultiHeadAttention(embed_size, num_heads, embed_size // num_heads, drop_out, device, v)
 
     def _forward(self, x, mask, opt_mask=None):
         batch, seq_len = x.shape
@@ -82,14 +80,14 @@ class DecoderBlock(Module):
         return output
 
 
-class TransformerDecoder(Module):
+class TransformerDecoder(ivy.Module):
     def __init__(self, tg_vocab_size, embed_size, mask,maxlength, num_stacks=6, num_heads=8, drop_out=0.1, device=None, v=None):
         self.device = device
-        self.embedding = Embedding(tg_vocab_size, embed_size)
-        self.pos_embedding = Embedding(maxlength, embed_size)
-        self.blocks = Sequential(
+        self.embedding = ivy.Embedding(tg_vocab_size, embed_size)
+        self.pos_embedding = ivy.Embedding(maxlength, embed_size)
+        self.blocks = ivy.Sequential(
             *[DecoderBlock(embed_size, mask, num_heads, drop_out, device, v) for _ in range(num_stacks)])
-        ivy.Module.__init__(
+        ivy.ivy.Module.__init__(
             self,
             device,
             v)
@@ -102,12 +100,12 @@ class TransformerDecoder(Module):
         return output
 
 
-class Transformer(Module):
+class Transformer(ivy.Module):
     def __init__(self, src_vocab_size, tg_vocab_size, embed_size, mask, maxlength, num_stacks=6, num_heads=8, drop_out=0.1, device=None, v=None):
         self.device = device
         self.encoder = TransformerEncoder(src_vocab_size, embed_size, num_stacks, num_heads, drop_out, maxlength, device, v)
         self.decoder = TransformerDecoder(tg_vocab_size, embed_size, mask, maxlength, num_stacks, num_heads, drop_out, device, v)
-        ivy.Module.__init__(
+        ivy.ivy.Module.__init__(
             self,
             device,
             v)
